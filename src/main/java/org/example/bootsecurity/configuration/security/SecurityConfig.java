7c87928240f7a545d9c5ecb22b21791a30e93b86
Jaloladdin-Kushnazarov <jalol2622@gmail.com>
2024-11-18 11:57:44 +0500

    second commit

--- src/main/java/org/example/bootsecurity/configuration/security/SecurityConfig.java
+++ src/main/java/org/example/bootsecurity/configuration/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,6 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, ObjectMapper objectMapper, AuthenticationEntryPoint authenticationEntryPoint) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest()
                         .fullyAuthenticated()
